370236f72002394a6c009fb1ec8c7f39921c9158
Mhaiyang <845879017@qq.com>
2018-09-16 15:28:14 +0800

    TaylorMei

--- train_mirror.py
+++ train_mirror.py
@@ -11,13 +11,13 @@
 import os
 import mirror
 # Need modify:
-import mrcnn.attention as modellib
+import mrcnn.attention2 as modellib
 
 # Root directory of the project
 ROOT_DIR = os.getcwd()
 
 # Directory to save logs and trained model
-MODEL_DIR = os.path.join(ROOT_DIR, "logs_attention")
+MODEL_DIR = os.path.join(ROOT_DIR, "logs_attention2")
 
 # Local path to trained weights file
 COCO_MODEL_PATH = os.path.join(ROOT_DIR, "mask_rcnn_coco.h5")
