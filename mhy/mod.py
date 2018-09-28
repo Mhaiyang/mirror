02cfd2e2684bc09129616a9d85dd48a95d749aeb
taylor <mhy845879017@gmail.com>
2018-09-29 04:24:29 +0800

    Taylor Mei

--- mhy/mod.py
+++ mhy/mod.py
@@ -2302,7 +2302,7 @@ class Mod():
         # Pre-defined layer regular expressions
         layer_regex = {
             # all layers but the backbone
-            "heads": r"(edge\_.*)|(rpn\_.*)|(fpn\_.*)",
+            "heads": r"(mod\_.*)|(rpn\_.*)|(fpn\_.*)",
             # From a specific Resnet stage and up
             "3+": r"(res3.*)|(bn3.*)|(res4.*)|(bn4.*)|(res5.*)|(bn5.*)|(mrcnn\_.*)|(rpn\_.*)|(fpn\_.*)",
             "4+": r"(res4.*)|(bn4.*)|(res5.*)|(bn5.*)|(mrcnn\_.*)|(rpn\_.*)|(fpn\_.*)",
