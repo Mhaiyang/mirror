12c25535a965c06877109c22f21b13916902fcda
Mhaiyang <845879017@qq.com>
2019-03-07 09:33:47 +0800

    TaylorMei

--- train_mirror.py
+++ train_mirror.py
@@ -14,7 +14,7 @@ import mirror
 import mhy.model as modellib
 
 # os.environ["CUDA_DEVICE_ORDER"] = "PCI_BUS_ID"
-os.environ["CUDA_VISIBLE_DEVICES"] = "0"
+os.environ["CUDA_VISIBLE_DEVICES"] = "1"
 
 # Root directory of the project
 ROOT_DIR = os.getcwd()
